aa05b7be223bc972da642e629c079900c247e401
Christoph Sommer <sommer@ccs-labs.org>
2018-04-10 11:14:57 +0200

    force appliance version to refer to a git commit (if possible)

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -exu
 
+echo '=> checking SHA1 sums'
 (cd files; shasum --algorithm 1 --check SHA1SUMS)
 
-packer build -on-error=ask instant-veins.json
+echo '=> attempting to auto-determine git version'
+SET_VERSION_A=
+SET_VERSION_B=
+VERSION=$(git describe --tags --match 'instant-veins-*' --always HEAD | sed -e 's/^instant-veins-//' || echo -ne '')
+if test -n ${VERSION}; then
+	SET_VERSION_A="-var"
+	SET_VERSION_B="version=${VERSION}"
+fi
+
+packer build ${SET_VERSION_A} ${SET_VERSION_B} -on-error=ask instant-veins.json
